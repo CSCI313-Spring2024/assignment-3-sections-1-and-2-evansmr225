4fe0eec52b76443474e15bbd51f6587f72a642b8
Matthew Evans <evansmr225@gmail.com>
2025-02-27 11:02:02 -0600

    Add an interpolation to a component's template

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,5 +11,19 @@ import {HousingLocation} from '../housinglocation';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+ };
 
+  constructor() {
+    console.log(this.housingLocation); // ✅ Add this to see if `housingLocation` exists at runtime
+  }
 }
